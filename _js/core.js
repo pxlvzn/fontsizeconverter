b21e76adfd4a287aefe0f4e06f4e64d101a50854
PXLVZN <pxlvzn@gmail.com>
2012-12-06 13:01:53 -0500

    Updated conversion options
    Added em and percent conversion options.
    Simplified the UI controls.

--- _js/core.js
+++ _js/core.js
@@ -3,41 +3,75 @@
  */
 
 // Namespace to avoid collisions
-var pxlvznTools = {
-    // Function to handle Ui events
-    addEvent : function(obj, evType, fn) {
-        if (obj.addEventListener) {
-            obj.addEventListener(evType, fn, false);
+var pxlvznTools= {
+    // Function to handle UI events
+    addEvent: function(obj,evType,fn) {
+        if(obj.addEventListener) {
+            obj.addEventListener(evType,fn,false);
             return true;
-            // 'Cuz MS sucks ballz!
-        } else if (obj.attachEvent) {
-            var forIE = obj.attachEvent("on" + evType, fn);
+            // and 'Cuz MS sucks ballz!
+        } else if(obj.attachEvent) {
+            var forIE=obj.attachEvent("on"+evType,fn);
             return forIE;
         } else {
             return false;
         }
     },
     // Pixel/Point Conversion Function
-    cvrtPxPt : function() {
-        var valueToConvert = document.getElementById('basesize').value;
-        var fontUnit = document.getElementById('fontunit').value;
-        var pxSize = valueToConvert * (96 / 72);
-        // Points to Pixels
-        var ptSize = valueToConvert * (72 / 96);
+    cvrtPxPt: function() {
+        // Baseline font unit is pixels
+        var baselineFontSize = 16;
+        // Number enbtered by the user
+        var valueToConvert=document.getElementById('basesize').value;
+        // conversion formula selected by the user
+        var convformula=document.getElementById('formula').value;
         // Pixels to Points
-        switch(fontUnit) {
-            case 'px':
-                document.getElementById('result').innerHTML = ptSize + 'pt';
+        var ptpxSize=valueToConvert*(96/72);
+        // Points to Pixels
+        var pxptSize=valueToConvert*(72/96);
+        // Pixels to EM
+        var pxemSize=valueToConvert/baselineFontSize;
+        // EM to Pixels
+        var empxSize=valueToConvert*baselineFontSize;
+        // Pixels to Percent
+        var pxPercentSize=100*(valueToConvert/baselineFontSize);
+        // Percent to Pixel
+        var percentPxSize=baselineFontSize*(valueToConvert/100);
+        // EM to Percent
+        var emPercentSize=valueToConvert*100;
+        // Percent to Em
+        var percentEmSize=valueToConvert/100;
+        //
+        switch(convformula) {
+            case 'empercent':
+                document.getElementById('result').innerHTML=emPercentSize+'&#37;';
                 break;
-            case 'pt':
-                document.getElementById('result').innerHTML = pxSize + 'px';
+            case 'empx':
+                document.getElementById('result').innerHTML=empxSize+'px';
+                break;
+            case 'percentem':
+                document.getElementById('result').innerHTML=percentEmSize+'em';
+                break;
+            case 'percentpx':
+                document.getElementById('result').innerHTML=percentPxSize+'px';
+                break;
+            case 'pxem':
+                document.getElementById('result').innerHTML=pxemSize+'em';
+                break;
+            case 'pxpercent':
+                document.getElementById('result').innerHTML=pxPercentSize+'&#37;';
+                break;
+            case 'pxpt':
+                document.getElementById('result').innerHTML=pxptSize+'pt';
+                break;
+            case 'ptpx':
+                document.getElementById('result').innerHTML=ptpxSize+'px';
                 break;
             default:
-            //
-            alert('Please select a font unit.')
+            // alert('Please select a font unit.')
         }
 
     }
 };
 //
-pxlvznTools.addEvent(document.getElementById('btn_convert'), 'click', pxlvznTools.cvrtPxPt);
+pxlvznTools.addEvent(document.getElementById('formula'),'change',pxlvznTools.cvrtPxPt);
