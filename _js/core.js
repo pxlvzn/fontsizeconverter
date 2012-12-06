65f14b16cb7ce0693ca55e99cb56a466c26533cc
pxlvzn <pxlvzn@gmail.com>
2012-12-05 23:49:18 -0500

    added font unit logic and UI elements

--- _js/core.js
+++ _js/core.js
@@ -3,28 +3,40 @@
  */
 
 // Namespace to avoid collisions
-var pxlvznTools={
+var pxlvznTools = {
     // Function to handle Ui events
-    addEvent: function(obj, evType, fn) {
-        if(obj.addEventListener) {
+    addEvent : function(obj, evType, fn) {
+        if (obj.addEventListener) {
             obj.addEventListener(evType, fn, false);
             return true;
             // 'Cuz MS sucks ballz!
-        } else if(obj.attachEvent) {
-            var forIE=obj.attachEvent("on"+evType, fn);
+        } else if (obj.attachEvent) {
+            var forIE = obj.attachEvent("on" + evType, fn);
             return forIE;
         } else {
             return false;
         }
     },
     // Pixel/Point Conversion Function
-    cvrtPxPt: function() {
+    cvrtPxPt : function() {
         var valueToConvert = document.getElementById('basesize').value;
-        var pxSize=valueToConvert*(96/72); // Points to Pixels
-        var ptSize=valueToConvert*(72/96); // Pixels to Points
-        //
-        document.getElementById('result').innerHTML=ptSize+'pt';
-        //document.getElementById('result').innerHTML=pxSize+'pt';
+        var fontUnit = document.getElementById('fontunit').value;
+        var pxSize = valueToConvert * (96 / 72);
+        // Points to Pixels
+        var ptSize = valueToConvert * (72 / 96);
+        // Pixels to Points
+        switch(fontUnit) {
+            case 'px':
+                document.getElementById('result').innerHTML = ptSize + 'pt';
+                break;
+            case 'pt':
+                document.getElementById('result').innerHTML = pxSize + 'px';
+                break;
+            default:
+            //
+            alert('Please select a font unit.')
+        }
+
     }
 };
 //
