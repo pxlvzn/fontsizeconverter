d45644969c7581a4785271ca1329935b340beb94
pxlvzn <pxlvzn@gmail.com>
2012-12-09 13:04:48 -0500

    Restructured conversion code based on suggestions from SirHacksAlot

--- _js/core.js
+++ _js/core.js
@@ -17,57 +17,49 @@ var pxlvznTools = {
             return false;
         }
     },
-    // Pixel/Point Conversion Function
+    // Font Unit Conversion Function
     convertUnit : function() {
         // Baseline font unit is pixels
         var baselineFontSize = 16;
-        // Number enbtered by the user
+        // Number entered by the user
         var valueToConvert = document.getElementById('basesize').value;
         // conversion formula selected by the user
         var convformula = document.getElementById('formula').value;
-        // Pixels to Points
-        var ptpxSize = valueToConvert * (96 / 72);
-        // Points to Pixels
-        var pxptSize = valueToConvert * (72 / 96);
-        // Pixels to EM
-        var pxemSize = valueToConvert / baselineFontSize;
-        // EM to Pixels
-        var empxSize = valueToConvert * baselineFontSize;
-        // Pixels to Percent
-        var pxPercentSize = 100 * (valueToConvert / baselineFontSize);
-        // Percent to Pixel
-        var percentPxSize = baselineFontSize * (valueToConvert / 100);
-        // EM to Percent
-        var emPercentSize = valueToConvert * 100;
-        // Percent to Em
-        var percentEmSize = valueToConvert / 100;
-
+        // Test to make sure that the input value is a number
         if (valueToConvert > 0) {
-            //
+            // Run selected conversion formula.
             switch(convformula) {
                 case 'empercent':
-                    document.getElementById('result').innerHTML = '<p>' + emPercentSize + '&#37;</p>';
+                    // EM to Percent
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert * 100) + '&#37;</p>';
                     break;
                 case 'empx':
-                    document.getElementById('result').innerHTML = '<p>' + empxSize + 'px</p>';
+                    // EM to Pixels
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert * baselineFontSize) + 'px</p>';
                     break;
                 case 'percentem':
-                    document.getElementById('result').innerHTML = '<p>' + percentEmSize + 'em</p>';
+                    // Percent to Em
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert / 100) + 'em</p>';
                     break;
                 case 'percentpx':
-                    document.getElementById('result').innerHTML = '<p>' + percentPxSize + 'px</p>';
+                    // Percent to Pixel
+                    document.getElementById('result').innerHTML = '<p>' + (baselineFontSize * (valueToConvert / 100)) + 'px</p>';
                     break;
                 case 'pxem':
-                    document.getElementById('result').innerHTML = '<p>' + pxemSize + 'em</p>';
+                    // Pixels to EM
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert / baselineFontSize) + 'em</p>';
                     break;
                 case 'pxpercent':
-                    document.getElementById('result').innerHTML = '<p>' + pxPercentSize + '&#37;</p>';
+                    // Pixels to Percent
+                    document.getElementById('result').innerHTML = '<p>' + (100 * (valueToConvert / baselineFontSize)) + '&#37;</p>';
                     break;
                 case 'pxpt':
-                    document.getElementById('result').innerHTML = '<p>' + pxptSize + 'pt</p>';
+                    // Pixel to Point
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert * (72 / 96)) + 'pt</p>';
                     break;
                 case 'ptpx':
-                    document.getElementById('result').innerHTML = '<p>' + ptpxSize + 'px</p>';
+                    // Point to Pixel
+                    document.getElementById('result').innerHTML = '<p>' + (valueToConvert * (96 / 72)) + 'px</p>';
                     break;
                 default:
                 // alert('Please select a font unit.')
@@ -76,15 +68,18 @@ var pxlvznTools = {
             document.getElementById('result').innerHTML = '<p class="error">Please enter a number.</p>';
         };
     },
+    // Function to clear input field of preset/default value and reset select menu index to zero.
     clearField : function() {
         this.value === this.defaultValue ? this.value = '' : null;
         document.getElementById('formula').selectedIndex = 0;
     },
+    // Function to reset input field to preset/default value
     resetField : function() {
         this.value === '' ? this.value = 'Enter a Number' : null;
     }
 };
-//
+// Initialze functions to call upon user action(s)
 pxlvznTools.addEvent(document.getElementById('basesize'), 'focus', pxlvznTools.clearField);
 pxlvznTools.addEvent(document.getElementById('basesize'), 'blur', pxlvznTools.resetField);
 pxlvznTools.addEvent(document.getElementById('formula'), 'change', pxlvznTools.convertUnit);
+//EOF
