b674c33fd6e91771b55cf81d3801359d468ca46f
pxlvzn <pxlvzn@gmail.com>
2012-12-08 21:47:10 -0500

    Added validation. UI enhancements

--- _js/core.js
+++ _js/core.js
@@ -18,7 +18,7 @@ var pxlvznTools = {
         }
     },
     // Pixel/Point Conversion Function
-    cvrtPxPt : function() {
+    convertUnit : function() {
         // Baseline font unit is pixels
         var baselineFontSize = 16;
         // Number enbtered by the user
@@ -41,39 +41,44 @@ var pxlvznTools = {
         var emPercentSize = valueToConvert * 100;
         // Percent to Em
         var percentEmSize = valueToConvert / 100;
-        //
-        switch(convformula) {
-            case 'empercent':
-                document.getElementById('result').innerHTML = emPercentSize + '&#37;';
-                break;
-            case 'empx':
-                document.getElementById('result').innerHTML = empxSize + 'px';
-                break;
-            case 'percentem':
-                document.getElementById('result').innerHTML = percentEmSize + 'em';
-                break;
-            case 'percentpx':
-                document.getElementById('result').innerHTML = percentPxSize + 'px';
-                break;
-            case 'pxem':
-                document.getElementById('result').innerHTML = pxemSize + 'em';
-                break;
-            case 'pxpercent':
-                document.getElementById('result').innerHTML = pxPercentSize + '&#37;';
-                break;
-            case 'pxpt':
-                document.getElementById('result').innerHTML = pxptSize + 'pt';
-                break;
-            case 'ptpx':
-                document.getElementById('result').innerHTML = ptpxSize + 'px';
-                break;
-            default:
-            // alert('Please select a font unit.')
-        }
 
+        if (valueToConvert > 0) {
+            //
+            switch(convformula) {
+                case 'empercent':
+                    document.getElementById('result').innerHTML = '<p>' + emPercentSize + '&#37;</p>';
+                    break;
+                case 'empx':
+                    document.getElementById('result').innerHTML = '<p>' + empxSize + 'px</p>';
+                    break;
+                case 'percentem':
+                    document.getElementById('result').innerHTML = '<p>' + percentEmSize + 'em</p>';
+                    break;
+                case 'percentpx':
+                    document.getElementById('result').innerHTML = '<p>' + percentPxSize + 'px</p>';
+                    break;
+                case 'pxem':
+                    document.getElementById('result').innerHTML = '<p>' + pxemSize + 'em</p>';
+                    break;
+                case 'pxpercent':
+                    document.getElementById('result').innerHTML = '<p>' + pxPercentSize + '&#37;</p>';
+                    break;
+                case 'pxpt':
+                    document.getElementById('result').innerHTML = '<p>' + pxptSize + 'pt</p>';
+                    break;
+                case 'ptpx':
+                    document.getElementById('result').innerHTML = '<p>' + ptpxSize + 'px</p>';
+                    break;
+                default:
+                // alert('Please select a font unit.')
+            };
+        } else {
+            document.getElementById('result').innerHTML = '<p class="error">Please enter a number.</p>';
+        };
     },
     clearField : function() {
         this.value === this.defaultValue ? this.value = '' : null;
+        document.getElementById('formula').selectedIndex = 0;
     },
     resetField : function() {
         this.value === '' ? this.value = 'Enter a Number' : null;
@@ -82,4 +87,4 @@ var pxlvznTools = {
 //
 pxlvznTools.addEvent(document.getElementById('basesize'), 'focus', pxlvznTools.clearField);
 pxlvznTools.addEvent(document.getElementById('basesize'), 'blur', pxlvznTools.resetField);
-pxlvznTools.addEvent(document.getElementById('formula'), 'change', pxlvznTools.cvrtPxPt);
+pxlvznTools.addEvent(document.getElementById('formula'), 'change', pxlvznTools.convertUnit);
