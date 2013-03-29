6eee6acd5eb075ed6a0be855e34874be2b43475f
PXLVZN <pxlvzn@gmail.com>
2013-03-29 16:01:35 -0400

    streamlined font size selection. Code clean up.

--- _js/core.js
+++ _js/core.js
@@ -4,32 +4,26 @@
 
 // Namespace to avoid collisions
 var pxlvznTools = {
-    baselineFontSize: 14,
-    // Function to handle UI events
-    addEvent : function(obj, evType, fn) {
-        "use strict";
+    addEvent : function(obj, evType, fn)
+    {"use strict";
         if (obj.addEventListener) {
             obj.addEventListener(evType, fn, false);
             return true;
-            // and 'Cuz MS sucks ballz!
+            // This is why we can't have nice things Microsoft!
         } else if (obj.attachEvent) {
             var forIE = obj.attachEvent("on" + evType, fn);
             return forIE;
         } else {
-            return false;}
+            return false;
+        }
     },
     // Font Unit Conversion Functionoints
-    convertUnit : function() {
-        "use strict";
-        // Baseline font unit is pixels
+    convertUnit : function()
+    {"use strict";
         var baselineFontSize = document.getElementById('baseline').value;
-        // Number entered by the user
         var valueToConvert = document.getElementById('basesize').value;
-        // conversion formula selected by the user
         var convformula = document.getElementById('formula').value;
-        // Test to make sure that the input value is a number
         if (valueToConvert > 0) {
-            // Run selected conversion formula.
             switch(convformula) {
                 case 'empercent':
                     // EM to Percent
@@ -64,30 +58,27 @@ var pxlvznTools = {
                     document.getElementById('result').innerHTML = '<p>' + (valueToConvert * (96 / 72)) + 'px</p>';
                     break;
                 default:
-                // alert('Please select a font unit.')
+                // console.log('Please select a font unit.')
             }
         } else {
             document.getElementById('result').innerHTML = '<p class="error">Please enter a number, only.</p>';
         }
     },
-    // Function to clear input field of preset/default value and reset select menu index to zero.
-    clearField : function() {
-        "use strict";
-        if(this.value !== ''){
+    clearField : function()
+    {"use strict";
+        if (this.value !== '') {
             this.value = '';
         }
         document.getElementById('formula').selectedIndex = 0;
-        document.getElementById('baseline').selectedIndex = 0;
     },
-    // Function to reset input field to preset/default value
-    resetField : function() {
-        "use strict";
-          if(this.value === ''){
+    resetField : function()
+    {"use strict";
+        if (this.value === '') {
             this.value = 'Enter a Number';
         }
     }
 };
-// Initialze functions to call upon user action(s)
+
 pxlvznTools.addEvent(document.getElementById('basesize'), 'focus', pxlvznTools.clearField);
 pxlvznTools.addEvent(document.getElementById('basesize'), 'blur', pxlvznTools.resetField);
 pxlvznTools.addEvent(document.getElementById('btn_convert'), 'click', pxlvznTools.convertUnit);
